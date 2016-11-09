4b1d79761ba68e2c6d82f8c64bf8bbb4b1cb1287
Tapani Rantakokko <tapani.rantakokko@finwe.fi>
2016-11-09 14:44:44 +0200

    First version

--- app/src/main/java/fi/finwe/orion360/sdk/pro/hello/MainActivity.java
+++ app/src/main/java/fi/finwe/orion360/sdk/pro/hello/MainActivity.java
@@ -1,13 +1,19 @@
 package fi.finwe.orion360.sdk.pro.hello;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import fi.finwe.orion360.SimpleOrionActivity;
+
+public class MainActivity extends SimpleOrionActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setOrionView(R.id.orion_view);
+
+        setContentUri("https://s3.amazonaws.com/orion360-us/Orion360_test_video_2d_equi_360x180deg_1920x960pix_30fps_30sec_x264.mp4");
     }
+
 }
