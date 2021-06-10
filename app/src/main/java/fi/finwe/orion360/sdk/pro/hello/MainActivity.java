230dc5507efe9b59ca1e9b783496425e1e623316
Tapani Rantakokko <tapani.rantakokko@finwe.fi>
2021-06-10 15:15:54 +0300

    Updated project files: - Now using the latest SDK versions and toolchain - Converted the project to use AndroidX - Removed jcenter bintray repo - Replaced Finwe bintray repo with Finwe GitHub repo

--- app/src/main/java/fi/finwe/orion360/sdk/pro/hello/MainActivity.java
+++ app/src/main/java/fi/finwe/orion360/sdk/pro/hello/MainActivity.java
@@ -9,10 +9,9 @@ public class MainActivity extends SimpleOrionActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
         setOrionView(R.id.orion_view);
-
         setContentUri("https://s3.amazonaws.com/orion360-us/Orion360_test_video_2d_equi_360x180deg_1920x960pix_30fps_30sec_x264.mp4");
     }
 
